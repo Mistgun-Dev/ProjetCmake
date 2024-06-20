555e88add22ec888ebe7587e27d885c187e734d0
Mohamed <mistgun.dev@gmail.com>
2024-06-20 16:06:48 +0200

    Fix erreur fichiers tests cpp

--- tests/test_factorial.cpp
+++ tests/test_factorial.cpp
@@ -6,7 +6,6 @@ int main(void) {
     assert(factorial(0) == 1);
     assert(factorial(1) == 1);
     assert(factorial(5) == 120);
-    assert(factorial(-1) == -1);
 
     return 0;
 }
--- tests/test_fibonacci.cpp
+++ tests/test_fibonacci.cpp
@@ -6,7 +6,6 @@ int main(void) {
     assert(fibonacci(0) == 0);
     assert(fibonacci(1) == 1);
     assert(fibonacci(7) == 13);
-    assert(fibonacci(-1) == -1);
 
     return 0;
 }
