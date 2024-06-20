8bd1deda02c84fc3b75288cb210e8c02c46be197
Mohamed <mistgun.dev@gmail.com>
2024-06-20 15:53:49 +0200

    Fix issues name functions in cpp files and headers files

--- src/fibonacci.cpp
+++ src/fibonacci.cpp
@@ -1,11 +1,11 @@
 #include "fibonacci.h"
 
-int fibbonacci(int n) {
+int fibonacci(int n) {
    if(n == 0){
       return 0;
    } else if(n == 1) {
       return 1;
    } else {
-      return (fibbonacci(n-1) + fibbonacci(n-2));
+      return (fibonacci(n-1) + fibonacci(n-2));
    }
 }
